fa9884a215b5a32f054edabf4efa90a79dda5247
Bhavyalakhani <bhavyalakhani89@gmail.com>
2021-08-25 18:13:10 +0530

    added:TTS, voice issue to be fixed

--- MyEyes/src/screens/Home.js
+++ MyEyes/src/screens/Home.js
@@ -1,25 +1,113 @@
 import React from "react";
-import { Text,TouchableOpacity,View,ScrollView, FlatList,StyleSheet } from "react-native";
+import { Text,TouchableOpacity,View,ScrollView, FlatList,StyleSheet, Alert, ActivityIndicator } from "react-native";
 import Icon from "react-native-vector-icons/FontAwesome";
 import Voice from '@react-native-community/voice';
+import Tts from 'react-native-tts';
 
 
 export default class Home extends React.Component {
     constructor(props){
         super(props);
-        Voice.onSpeechStart = this.voiceCommandActivate.bind(this);
+        Voice.onSpeechStart = this.onSpeechStart.bind(this);
+        Voice.onSpeechRecognized = this.onSpeechRecognized.bind(this);
+        Voice.onSpeechResults = this.onSpeechResults.bind(this);
+        Voice.onSpeechEnd = this.onSpeechEnd.bind(this);
         this.state={
-            "value":null
+            recognized: '',
+            started: '',
+            results: [],
+            activatevoice:true,
+            isloading:true
         }
     }
 
-    voiceCommandActivate = async () => {
-        console.log("Inside activate tfunction")
-        await Voice.start("en-US");
+    componentWillUnmount() {
+        Voice.destroy().then(Voice.removeAllListeners);
+      }
 
+      componentDidMount(){
+          this.setState({
+              isloading:false
+          })
+          Tts.speak('Welcome To MyEyes!', {
+            androidParams: {
+              KEY_PARAM_PAN: -1,
+              KEY_PARAM_VOLUME: 0.5,
+              KEY_PARAM_STREAM: 'STREAM_MUSIC',
+              language:"en-US",
+            },
+          });
+      }
+
+
+    onSpeechStart = (e) => {
+        console.log(e.value);
+        this.setState({
+          started: '√',
+        });
+      }
+
+      onSpeechEnd = (e) => {
+        console.log(e);
+      }
+
+    onSpeechRecognized = (e) => {
+        this.setState({
+          recognized: '√',
+        });
+      }
+
+    onSpeechResults = (e) =>  {
+        console.log(e.value)
+        this.setState({
+          results: e.value,
+        });
+      }
+
+     _startRecognition = async (e) => {
+        console.log("recognizing speech")
+        await Tts.speak('Please speak now', {
+                androidParams: {
+                KEY_PARAM_PAN: -1,
+                KEY_PARAM_VOLUME: 0.5,
+                KEY_PARAM_STREAM: 'STREAM_MUSIC',
+                language:"en-US"
+            },
+          });
+        this.setState({
+          recognized: '',
+          started: '',
+          results: [],
+          activatevoice:false
+        });
+        try {
+          await Voice.start('en-US');
+          console.log("yes")
+        } catch (e) {
+          console.error(e);
+        }
+      }
+
+    stoprecognition =  async () => {
+        this.setState({
+            activatevoice:true
+        });
+        console.log("Recognition stopped");
+        try {
+            await Voice.stop();
+        } catch (error) {
+            console.log(error);
+        }
     }
 
     render(){
+        if(this.state.isloading){
+            return(
+                <View>
+                    <ActivityIndicator size="large" color="#fccb45"></ActivityIndicator>
+                </View>
+            )
+        }
         return(
             <View style={{flex:1,flexDirection:"column",justifyContent:"space-between"}}>
                 <View>
@@ -39,12 +127,24 @@ export default class Home extends React.Component {
                         <Text style={styles.textsize}>Money Detection</Text>
                         <Icon name="angle-right" size={30} color="black" />
                     </TouchableOpacity>
+                    {
+                        this.state.results.map((result, index) => <Text style={styles.transcript}> {result}</Text>
+                    )}
                 </View>
-                <View>
-                    <TouchableOpacity style={styles.buttonbottom} onPress={this.voiceCommandActivate}>
-                        <Text style={{fontSize:20}}>Activate Voice Command</Text>
-                    </TouchableOpacity>
-                </View>
+                {
+                    this.state.activatevoice?
+                    <View>
+                        <TouchableOpacity style={styles.buttonbottom} onPress={this._startRecognition.bind(this)}>
+                            <Text style={{fontSize:20}}>Activate Voice Command</Text>
+                        </TouchableOpacity>
+                    </View>
+                    :
+                    <View>
+                        <TouchableOpacity style={styles.buttonbottom} onPress={this.stoprecognition.bind()}>
+                            <Text style={{fontSize:20}}>DeActivate Voice Command</Text>
+                        </TouchableOpacity>
+                    </View>
+                }
             </View>
         )
     }
--- MyEyes/src/screens/ModelOutput.js
+++ MyEyes/src/screens/ModelOutput.js
@@ -1,12 +1,19 @@
 import React, { Component } from 'react'
 import { Text, View,TouchableOpacity } from 'react-native'
+import Tts from 'react-native-tts';
 
 export class ModelOutput extends Component {
+
+
+    gotohome = () => {
+        console.log('inside gothome');
+        this.props.navigation.navigate("MyEyes");
+    }
     render() {
         return (
             <View>
                 <Text> ModelOutput Screen </Text>
-                <TouchableOpacity onPress={() => {this.props.navigation.navigate("Home")}}>
+                <TouchableOpacity onPress={this.gotohome}>
                     <Text>To Home</Text>
                 </TouchableOpacity>
             </View>
